4f435c1d4c46d9e6866aa5c746bbeb23034296e7
veldeta <terminatorslava38@gmail.com>
2025-07-17 22:37:52 +0300

    Добавил не большую логику и файл гит игнор, который игнорирует директорию vendor

--- scr/main/Main.java
+++ scr/main/Main.java
@@ -1,9 +1,19 @@
 package scr.main;
 
+import java.util.Random;
+
 class Main 
 {
     public static void main(String[] args) 
     {
         System.out.println("Я прогер, хехе");
+
+        int[] arr = {1,2,3,14,6};
+
+        for (int num : arr) {
+            Random ran = new Random();
+            int x = ran.nextInt(1,100);
+            System.out.println(num + " + " + x + " = " +  (num + x));
+        }
     }
 }
